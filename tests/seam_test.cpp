e271eeb7a5f3ef7acb699ccbbbd681d510d7c967
mxheller <heller.max1@gmail.com>
2019-01-07 10:56:07 -0500

    More flexible cmake

--- tests/seam_test.cpp
+++ tests/seam_test.cpp
@@ -11,10 +11,18 @@ TEST_CASE("Seam Instantiation") {
 
 TEST_CASE("Seam Updating") {
     Seam s(10);
-    s.update(1, 1);
+    s.update(4, 1);
     REQUIRE(s.getPositions().size() == 1);
+    REQUIRE(s.getPositions().at(0) == 4);
     REQUIRE(s.getEnergy() == 1);
     s.update(2, 2);
     REQUIRE(s.getPositions().size() == 2);
+    REQUIRE(s.getPositions().at(1) == 2);
     REQUIRE(s.getEnergy() == 3);
+}
+TEST_CASE("Seam Comparison") {
+    Seam s1(10), s2(10);
+    s1.update(5, 10.2);
+    s2.update(4, 4.5);
+    REQUIRE(s2 < s1);
 }
