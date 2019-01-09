2f0d1be27575801841b94c339e05dabcc432ccec
mxheller <heller.max1@gmail.com>
2019-01-09 16:57:55 -0500

    Seam removal testing

--- tests/seam_test.cpp
+++ tests/seam_test.cpp
@@ -20,10 +20,59 @@ TEST_CASE("Seam Updating") {
     REQUIRE(s.getPositions().at(1) == 2);
     REQUIRE(s.getEnergy() == 3);
 }
+
 TEST_CASE("Seam Comparison") {
     Seam s1(10), s2(10);
     s1.update(5, 10.2);
     s2.update(4, 4.5);
     REQUIRE(s2 < s1);
     REQUIRE(!(s1 < s2));
+}
+
+TEST_CASE("Seam Removal") {
+    CImg<unsigned char> img(5,3,1,3);
+    Seam s(3);
+    s.update(2,10.0);
+    s.update(2,10.0);
+    s.update(2,10.0);
+    auto new_img = s.removeFrom(img);
+    REQUIRE(new_img(0,0,0,0) == img(0,0,0,0));
+    REQUIRE(new_img(0,0,0,1) == img(0,0,0,1));
+    REQUIRE(new_img(0,0,0,2) == img(0,0,0,2));
+    REQUIRE(new_img(0,1,0,0) == img(0,1,0,0));
+    REQUIRE(new_img(0,1,0,1) == img(0,1,0,1));
+    REQUIRE(new_img(0,1,0,2) == img(0,1,0,2));
+    REQUIRE(new_img(0,2,0,0) == img(0,2,0,0));
+    REQUIRE(new_img(0,2,0,1) == img(0,2,0,1));
+    REQUIRE(new_img(0,2,0,2) == img(0,2,0,2));
+
+    REQUIRE(new_img(1,0,0,0) == img(1,0,0,0));
+    REQUIRE(new_img(1,0,0,1) == img(1,0,0,1));
+    REQUIRE(new_img(1,0,0,2) == img(1,0,0,2));
+    REQUIRE(new_img(1,1,0,0) == img(1,1,0,0));
+    REQUIRE(new_img(1,1,0,1) == img(1,1,0,1));
+    REQUIRE(new_img(1,1,0,2) == img(1,1,0,2));
+    REQUIRE(new_img(1,2,0,0) == img(1,2,0,0));
+    REQUIRE(new_img(1,2,0,1) == img(1,2,0,1));
+    REQUIRE(new_img(1,2,0,2) == img(1,2,0,2));
+
+    REQUIRE(new_img(2,0,0,0) == img(3,0,0,0));
+    REQUIRE(new_img(2,0,0,1) == img(3,0,0,1));
+    REQUIRE(new_img(2,0,0,2) == img(3,0,0,2));
+    REQUIRE(new_img(2,1,0,0) == img(3,1,0,0));
+    REQUIRE(new_img(2,1,0,1) == img(3,1,0,1));
+    REQUIRE(new_img(2,1,0,2) == img(3,1,0,2));
+    REQUIRE(new_img(2,2,0,0) == img(3,2,0,0));
+    REQUIRE(new_img(2,2,0,1) == img(3,2,0,1));
+    REQUIRE(new_img(2,2,0,2) == img(3,2,0,2));
+
+    REQUIRE(new_img(3,0,0,0) == img(4,0,0,0));
+    REQUIRE(new_img(3,0,0,1) == img(4,0,0,1));
+    REQUIRE(new_img(3,0,0,2) == img(4,0,0,2));
+    REQUIRE(new_img(3,1,0,0) == img(4,1,0,0));
+    REQUIRE(new_img(3,1,0,1) == img(4,1,0,1));
+    REQUIRE(new_img(3,1,0,2) == img(4,1,0,2));
+    REQUIRE(new_img(3,2,0,0) == img(4,2,0,0));
+    REQUIRE(new_img(3,2,0,1) == img(4,2,0,1));
+    REQUIRE(new_img(3,2,0,2) == img(4,2,0,2));
 }
